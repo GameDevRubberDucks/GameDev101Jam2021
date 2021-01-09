3cc8a99295ec0d82a5523679b01787f1245c612b
Bo <Wen.yu@ontariotechu.net>
2021-01-09 14:10:25 -0500

    [ADD] dialogue now can be triggered by the players

---
+++ GameDev101/Assets/Studio Assets/Scripts/Triggers/Interactions/ActionDialogue.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class ActionDialogue : MonoBehaviour
+{
+    public bool dialogueTrigger;
+
+    private DialogueRunner dialogueRunner;
+    private NPC npc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueRunner = FindObjectOfType<DialogueRunner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        npc = other.GetComponent<NPC>();
+
+        if (!string.IsNullOrEmpty(npc.talkToNode) && dialogueTrigger) // npc has a conversation node and that dialogue 
+        {
+            //disables dialogue trigger so dialogue runner does not starts the dialogue again while running.
+           dialogueTrigger = false;
+           dialogueRunner.StartDialogue(npc.talkToNode);
+            
+        }
+    }
+}
